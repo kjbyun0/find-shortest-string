c088558850eb9465e3ffe3c88cab3bbf9e6f8522
KI JONG BYUN <kjbyun0@gmail.com>
2024-08-24 12:37:33 -0500

    Add my solution

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,11 @@
 function findShortestString(arr) {
   // type your code here
+  let minStr = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length < minStr.length)
+      minStr = arr[i];
+  }
+  return minStr;
 }
 
 if (require.main === module) {
@@ -17,7 +23,49 @@ if (require.main === module) {
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
 
+  console.log("");
+
+  console.log("Expecting: 'cat'");
+  console.log("=>", findShortestString(['cat']));
+
+
   // BENCHMARK HERE
+  
+  function getRandomNum(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  function getRandomStr(len) {
+    let randomStr = '';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i < len; i++) {
+      randomStr = randomStr.concat(alphabet[getRandomNum(alphabet.length)]);
+    }
+
+    // console.log("getRandomStr, len: ", len, ", randomStr: ", randomStr);
+
+    return randomStr;
+  }
+
+  const longInput = [];
+  for (let i = 0; i < 100; i++) {
+    longInput.push(getRandomStr(getRandomNum(10)));
+  }
+  // console.log(longInput);
+
+  const shortInput = [];
+  for (let i = 0; i < 3; i++) {
+    shortInput.push(getRandomStr(getRandomNum(10)));
+  }
+  // console.log(shortInput);
+
+  const startTime = Date.now();
+  for (let i = 0; i < 1000; i++) {
+    findShortestString(shortInput);
+    findShortestString(longInput);
+  }
+  const endTime = Date.now();
+  console.log("Average Runtime: ", (endTime - startTime) / 2000);
 }
 
 module.exports = findShortestString;
